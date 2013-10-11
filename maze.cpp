5cd4d01ed7d0d1d75e3559ea4af1caaed72f1460
Richard Habeeb <richard.habeeb@gmail.com>
2013-10-10 22:05:56 -0500

    Initial commit

---
+++ maze.cpp
@@ -0,0 +1,11 @@
+#include "maze.h"
+
+
+maze::maze(void)
+{
+}
+
+
+maze::~maze(void)
+{
+}
---
+++ pathfinding.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+#include "maze.h"
+
+
+
+int main()
+{
+	printf("Hello, World!");
+
+	return 0;
+}
+
+
